1e2dd4fb7cfb8e85e9a3c7026a94de9e6d7c4a8f
GitHub Action <actions@github.com>
2025-08-08 08:49:58 +0000

    🔧 cs-fixer: auto fix coding standards

--- src/Detector/IPDetector.php
+++ src/Detector/IPDetector.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace NDevsEu\GeoIp\Detector;
@@ -7,43 +8,44 @@ use Symfony\Component\HttpFoundation\Request;
 
 class IPDetector
 {
-	public static function getRealClientIp(Request $request): ?string
-	{
-		$forwardedFor = $request->headers->get('X-Forwarded-For');
-		if ($forwardedFor) {
-			$ips = explode(',', $forwardedFor);
-			foreach ($ips as $ip) {
-				$ip = trim($ip);
-				if (self::isPublicIp($ip)) {
-					return $ip;
-				}
-			}
-		}
+    public static function getRealClientIp(Request $request): ?string
+    {
+        $forwardedFor = $request->headers->get('X-Forwarded-For');
+        if ($forwardedFor) {
+            $ips = explode(',', $forwardedFor);
+            foreach ($ips as $ip) {
+                $ip = trim($ip);
+                if (self::isPublicIp($ip)) {
+                    return $ip;
+                }
+            }
+        }
 
-		foreach (['X-Client-IP', 'Forwarded', 'CF-Connecting-IP'] as $header) {
-			$ip = $request->headers->get($header);
-			if ($ip && self::isPublicIp($ip)) {
-				return $ip;
-			}
-		}
+        foreach (['X-Client-IP', 'Forwarded', 'CF-Connecting-IP'] as $header) {
+            $ip = $request->headers->get($header);
+            if ($ip && self::isPublicIp($ip)) {
+                return $ip;
+            }
+        }
 
-		$ip = $request->getClientIp();
-		if ($ip && self::isPublicIp($ip)) {
-			return $ip;
-		}
+        $ip = $request->getClientIp();
+        if ($ip && self::isPublicIp($ip)) {
+            return $ip;
+        }
 
-		return null;
-	}
-	public static function isPublicIp(string $ip): bool
-	{
-		if (!filter_var($ip, \FILTER_VALIDATE_IP)) {
-			return false;
-		}
+        return null;
+    }
 
-		return !filter_var(
-			$ip,
-			\FILTER_VALIDATE_IP,
-			\FILTER_FLAG_NO_PRIV_RANGE | \FILTER_FLAG_NO_RES_RANGE
-		);
-	}
+    public static function isPublicIp(string $ip): bool
+    {
+        if (!filter_var($ip, \FILTER_VALIDATE_IP)) {
+            return false;
+        }
+
+        return !filter_var(
+            $ip,
+            \FILTER_VALIDATE_IP,
+            \FILTER_FLAG_NO_PRIV_RANGE | \FILTER_FLAG_NO_RES_RANGE
+        );
+    }
 }
